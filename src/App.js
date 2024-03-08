a2c0bb2b55a2d90e2124dcb3f0c640056d5a23fa
Jay Koutavas <jay@heynow.com>
2024-03-08 16:03:51 -0500

    WDYR biglist rendering example (see  sandbox: http://bit.ly/wdyr-sb)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import React from "react";
+import { times } from "lodash";
 
-const App = () =>{
-    return (
-        <h1>
-            Welcome to React App thats build using Webpack and Babel separately
-        </h1>
-    )
-}
+/**
+ *
+ *
+ *
+ *
+ * Notice that this sandbox will not work with React 17 or above
+ * because of how codesandbox.io implements requires of packages.
+ *
+ * sandbox: http://bit.ly/wdyr-sb
+ *
+ *
+ */
+const BigListPureComponent = React.memo(({ style }) => {
+  console.log(
+    "BigListPureComponent Re-Render! - We don't want this to happen too often."
+  );
+  return (
+    <div style={style}>
+      <h2>BigListPureComponent</h2>
+      <div>
+        {times(3000).map((n) => (
+          <div key={n}>Element #{n}</div>
+        ))}
+      </div>
+    </div>
+  );
+});
 
-export default App
+BigListPureComponent.displayName = "BigListPureComponent";
+
+const App = () => {
+  const [count, setCount] = React.useState(0);
+
+  /* use the hook instead of the const to prevent
+   ** "BigListPureComponent" from re-rendering wit this component */
+
+  //const bigListStyle = React.useMemo(() => ({ width: "100%" }), []);
+  const bigListStyle = { width: "100%" };
+
+  return (
+    <div className="Main">
+      <h1>Big List (Main Demo)</h1>
+      <p>
+        Open the console and notice how the heavy list re-renders on every click
+        on "Increase!" even though its props are the same.
+      </p>
+      <div>
+        <button onClick={() => setCount((c) => c + 1)}>
+          "Increase!" Count: {count}
+        </button>
+      </div>
+      <BigListPureComponent style={bigListStyle} />
+    </div>
+  );
+};
+
+export default App;
