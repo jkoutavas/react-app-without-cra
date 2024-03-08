316e4ffadf76d4ab35393cf2aacebc6b73fd8c9a
Jay Koutavas <jay@heynow.com>
2024-03-08 11:09:13 -0500

    Updated React 17 to React 18

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import React from "react";
-import reactDom from "react-dom";
-import App from "./src/App"
+import { createRoot } from "react-dom/client";
+import App from "./src/App";
 
-reactDom.render(<App />, document.getElementById("root"));
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
